08451fb9f0a93e1be0027c26bdce30b043c6ce54
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-01 01:19:12 +0300

    fix

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import { UseTg } from "../../hooks/UseTg";
-import "../Header/Header.css";
+//import "../Header.css";
 
 const Header = () => {
 
