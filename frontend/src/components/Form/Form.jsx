f2402b5640880903370553059bae5269d95d8764
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-03 22:49:35 +0300

    fix issues

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,66 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Form/Form.css";
 import { UseTg } from "../../hooks/UseTg";
-import { useEffect } from "react";
 
 const Form = () => {
+  const [country, setCountry] = useState('');
+  const [street, setStreet] = useState('');
+  const [subject, setSubject] = useState('');
 
-    const [country, setCoutry] = useState('');
-    const [street, setStreet] = useState('');
-    const [subject, setSubject] = useState('');
+  const onChangeCountry = (el) => {
+    setCountry(el.target.value);
+  }
 
-    const onChangeCountry = (el) => {
-        setCoutry(el.target.value);
+  const onChangeStreet = (el) => {
+    setStreet(el.target.value);
+  }
+
+  const onChangeSubject = (el) => {
+    setSubject(el.target.value);
+  }
+
+  const { tg } = UseTg();
+
+  useEffect(() => {
+    if (tg) {
+      tg.MainButton.setParams({
+        text: "Send data"
+      });
     }
+  }, [tg]);
 
-    const onChangeStreet = (el) => {
-        setStreet(el.target.value)
+  useEffect(() => {
+    if (!country || !street) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
     }
+  }, [country, street, tg]);
 
-    const onChangeSubject = (el) => {
-        setSubject(el.target.value)
-    }
-
-    const {tg} = UseTg();
-
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: "Send data"
-        })
-    }, [])
-
-    useEffect(() => {
-        if(!country || !street){
-            tg.MainButton.hide()
-        } else {
-            tg.MainButton.show()
-        }
-    }, [country, street]) 
-
-
-    return(
-        <div className={"form"}>
-            <h3>Input your data:</h3>
-            <input 
-            className={"input"} 
-            type="text" 
-            placeholder="Coutry"
-            value={country}
-            onChange={onChangeCountry}
-            />
-            <input 
-            className={"input"} 
-            type="text" 
-            placeholder="Street"
-            value={street}
-            onChange={onChangeStreet}
-            />
-            <select value={subject} onChange={onChangeSubject} className={"select"}>
-                <option value={"physical"}>Individual</option>
-                <option value={"legal"}>Legal entity</option>
-            </select>
-        </div>
-    )
+  return (
+    <div className={"form"}>
+      <h3>Input your data:</h3>
+      <input 
+        className={"input"} 
+        type="text" 
+        placeholder="Country"
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input 
+        className={"input"} 
+        type="text" 
+        placeholder="Street"
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select value={subject} onChange={onChangeSubject} className={"select"}>
+        <option value={"physical"}>Individual</option>
+        <option value={"legal"}>Legal entity</option>
+      </select>
+    </div>
+  )
 }
 
 export default Form;
