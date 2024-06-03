03e9496a8b765415309a62821af74f17581baf78
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-03 19:21:11 +0300

    add styles to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import { UseTg } from "../../hooks/UseTg";
+import "../Header/Header.css"
 
 const Header = () => {
   const { user, onClose } = UseTg();
@@ -13,9 +14,6 @@ const Header = () => {
       <span className={'username'}>
         {user ? user.username + " lol" : "here should be username"}
       </span>
-      <span className={"id"}>
-        {user ? user.id + " kek" : "here should be id"}
-        </span>
     </div>
   );
 }
