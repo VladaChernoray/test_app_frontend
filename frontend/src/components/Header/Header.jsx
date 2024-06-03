75fe9cc75617b93b831623929e0b0499a55a738f
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-03 17:00:11 +0300

    fix issues

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Button from "../Button/Button";
 import { UseTg } from "../../hooks/UseTg";
-//import "../Header.css";
+// import "../Header.css";
 
 const Header = () => {
+  const { user, onClose } = UseTg();
 
-const {user, onClose} = UseTg();
-
-    return(
-        <div className={"header"}>
-            <Button onClick={onClose}>Close</Button>
-            <span className={'username'}>{user?.username + "lol"}</span>
-        </div>
-    )
+  return (
+    <div className={"header"}>
+      <Button onClick={onClose}>Close</Button>
+      <span className={'username'}>
+        {user ? user.username + " lol" : "Loading..."}
+      </span>
+    </div>
+  );
 }
 
 export default Header;
