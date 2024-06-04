850dd1cedc952f45a6d4636b5ab1b12343f6e4b5
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-04 19:54:52 +0300

    change styles

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -2,12 +2,12 @@ const products = [
     { id: '1', img: "https://www.svgrepo.com/show/425702/potato-snack-food.svg", title: 'Potato Snack', price: 5, description: 'Blue color, straight' },
     { id: '2',img: "https://www.svgrepo.com/show/425703/food-food-and-restauran-restauran-menu.svg", title: 'Hot-dog', price: 4, description: 'Green color, warm' },
     { id: '3',img: "https://www.svgrepo.com/show/425704/pie-dessert-food.svg", title: 'Pie', price: 5, description: 'Blue color, straight' },
-    { id: '4',img: "https://www.svgrepo.com/show/425705/donut-food-food-and-restauran.svg", title: 'Donut', price: 12, description: 'Green color, warm' },
+    { id: '4',img: "https://www.svgrepo.com/show/425705/donut-food-food-and-restauran.svg", title: 'Donut', price: 8, description: 'Green color, warm' },
     { id: '5',img: "https://www.svgrepo.com/show/425706/ice-cream-ice-food.svg", title: 'Ice Cream', price: 5, description: 'Blue color, straight' },
     { id: '6',img: "https://www.svgrepo.com/show/425707/wrap-snack-food.svg", title: 'Wrap Snack', price: 6, description: 'Green color, warm' },
     { id: '7',img: "https://www.svgrepo.com/show/425708/burger-hamburger-food.svg", title: 'Burger Hamburger', price: 5, description: 'Blue color, straight' },
-    { id: '8', img: "https://www.svgrepo.com/show/425709/fried-chicken-food-food-and-restauran.svg",title: 'Fried Chicken', price: 12, description: 'Green color, warm' },
-    { id: '9', img: "https://www.svgrepo.com/show/425710/french-fries-dessert-snack.svg",title: 'French Fries', price: 12, description: 'Green color, warm' }
+    { id: '8', img: "https://www.svgrepo.com/show/425709/fried-chicken-food-food-and-restauran.svg",title: 'Fried Chicken', price: 9, description: 'Green color, warm' },
+    { id: '9', img: "https://www.svgrepo.com/show/425710/french-fries-dessert-snack.svg",title: 'French Fries', price: 9, description: 'Green color, warm' },
   ];
   
 export default products;
